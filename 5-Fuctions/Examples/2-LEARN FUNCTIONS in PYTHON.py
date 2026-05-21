957ebc97fd8e4756f09824963dfac2b1a1b47f71
AlejandroMarquez1 <alejandroyahirmarquez@gmail.com>
2026-05-20 18:27:44 -0600

    Second commit

---
+++ 5-Fuctions/Examples/2-LEARN FUNCTIONS in PYTHON.py
@@ -0,0 +1,34 @@
+# LEARN FUNCTIONS in PYTHON def, pass, sintaxis, None, return vs print, argumentos, scope y más
+
+"""
+def hello():
+    print("Hello")
+
+hello()
+"""
+
+"""
+def hello(name):
+    print(f"Hello {name}")
+
+hello("Alex")
+"""
+
+"""
+def average (num1, num2, num3):
+    return  num1 + num2 + num3 / 3
+
+call = average(1,2,3)
+print(call)
+print(average(1, 2, 3))
+"""
+
+"""
+def subtraction(num1 = 3, num2 = 2):
+    return num1 - num2
+print(f"With default values: {subtraction()}")
+print(f"Using the single-value order: {subtraction(5)}")
+print(f"Using the order with two arguments: {subtraction(5,3)}")
+print(f"With Keyword arguments: {subtraction(num1 = 5, num2 = 3)}")
+print(f"With only Keyword argument: {subtraction(num2 = 3)}")
+"""
