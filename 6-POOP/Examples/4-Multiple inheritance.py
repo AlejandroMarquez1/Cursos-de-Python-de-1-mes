3df4e6cd6467fb6d0512d7dce26179916bce7ddf
AlejandroMarquez1 <alejandroyahirmarquez@gmail.com>
2026-05-22 19:44:47 -0600

    fifth commit

---
+++ 6-POOP/Examples/4-Multiple inheritance.py
@@ -0,0 +1,37 @@
+# Multiple inheritance
+
+# multiple inheritance = inherit from come that one parent class
+# C(A,B)
+# multilevel inheritance = inherit from a parent witch inherits from another parent
+# C(B) <- B(A) <- A
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def eat(self):
+        print(f"{self.name} is eating")
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Bugs")
+fish = Fish("Tony")
+hawk = Hawk("Nemo")
+
+fish.flee()
